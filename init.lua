1d29c989cacf0b14d0c2c852a4f986ed660cf8a4
nimeshmaharjan1 <maharjannimesh11@gmail.com>
2022-11-22 22:42:34 +0545

    chore(config): added all the necessary configs

--- init.lua
+++ init.lua
@@ -2,4 +2,13 @@ require "user.options"
 require "user.keymaps" 
 require "user.plugins" 
 require "user.cmp"
-vim.cmd "colorscheme onedark" 
+require "user.lsp" 
+require "user.telescope" 
+require "user.treesitter"
+require "user.comment"
+require "user.nvim-tree"
+require "user.lualine"
+require "user.toggleterm"
+
+vim.cmd [[colorscheme tokyonight-moon]] 
+
