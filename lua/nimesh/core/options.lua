1b19f992921493cec4f8f77afa1a7ae04079ea4f
nimesh.maharjan <nimesh.maharjan@ekbana.info>
2023-04-05 16:05:38 +0545

    options and keymaps with some plugins :zap:

---
+++ lua/nimesh/core/options.lua
@@ -0,0 +1,31 @@
+local opt = vim.opt
+
+opt.number = true
+opt.relativenumber = true
+
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.autoindent = true
+
+opt.wrap = true
+
+opt.ignorecase = true
+opt.smartcase = true
+
+opt.cursorline = true
+
+opt.termguicolors = true
+opt.background = "dark"
+opt.signcolumn = "yes"
+
+opt.backspace = "indent,eol,start"
+
+opt.clipboard:append("unnamedplus")
+
+opt.splitright = true
+opt.splitbelow = true
+
+opt.iskeyword:append("-")
+
+--vim-maximizer
